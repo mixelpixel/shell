fb08aa597c22894cc33b65e20a900e1a75acc433
mixelpixel <mixelpixel@users.noreply.github.com>
2016-09-18 02:49:08 -0400

    Add files via upload

---
+++ nap.sh
@@ -0,0 +1,8 @@
+# My first shell script!
+# per http://askubuntu.com/questions/1792/how-can-i-suspend-hibernate-from-command-line
+
+dbus-send --system --print-reply \
+    --dest="org.freedesktop.UPower" \
+    /org/freedesktop/UPower \
+    org.freedesktop.UPower.Suspend
+
